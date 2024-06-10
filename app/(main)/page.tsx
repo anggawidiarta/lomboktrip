9f40b7ffe377505fa094fc5f05f208f081f1ce9c
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-10 23:18:49 +0800

    chore: init page and component

---
+++ app/(main)/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Home: NextPage = () => {
+  return <main className="max-container text-4xl">Home</main>;
+};
+
+export default Home;
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return <nav className="border-2 text-white text-2xl border-green-50">nav</nav>;
+};
+
+export default Navbar;
