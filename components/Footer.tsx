963d709bd5ad46cd779a49bf2200ee2d7b978ea7
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-12 15:43:33 +0800

    chore: adjust logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,11 +23,16 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
 
 const Footer = () => {
   return (
-    <footer className=" mb-5 flexCenter">
+    <footer className="mb-5 flexCenter">
       <div className="flex flex-col w-full gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+            <p className="text-3xl poppins-bold">
+              TripIn
+              <span className="transition-all duration-150 text-green-50 hover:text-green-700">
+                Lombok.
+              </span>
+            </p>
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
@@ -35,7 +40,11 @@ const Footer = () => {
               <FooterColumn title={columns.title} key={index}>
                 <ul className="flex flex-col gap-4 regular-14 text-gray-30">
                   {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                    <Link
+                      href="/"
+                      key={link}
+                      className="transition-all duration-150 hover:text-green-50"
+                    >
                       {link}
                     </Link>
                   ))}
@@ -49,10 +58,10 @@ const Footer = () => {
                   <Link
                     href="/"
                     key={link.label}
-                    className="flex gap-4 md:flex-col lg:flex-row"
+                    className="flex items-center gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className="whitespace-nowrap">{link.label}:</p>
-                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                    <p className=" whitespace-nowrap">{link.label}:</p>
+                    <p className="transition-all duration-150 hover:text-green-50 medium-14 whitespace-nowrap text-blue-70">
                       {link.value}
                     </p>
                   </Link>
@@ -76,7 +85,7 @@ const Footer = () => {
 
         <div className="border bg-gray-20" />
         <p className="w-full text-center regular-14 text-gray-30">
-          2023 Hilink | All rights reserved
+          {new Date().getFullYear()} TripInLombok | All rights reserved
         </p>
       </div>
     </footer>
