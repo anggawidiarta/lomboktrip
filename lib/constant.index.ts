479b70b28432ba018280e8c6c5389c56b373c928
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-13 09:07:44 +0800

    chore: add destinations constant

--- lib/constant.index.ts
+++ lib/constant.index.ts
@@ -76,3 +76,126 @@ export const SOCIALS = {
   title: "Social Media",
   links: ["/facebook.svg", "/instagram.svg", "/youtube.svg"],
 };
+
+// Destinations
+// constants.js
+export const DESTINATIONS = [
+  {
+    id: 1,
+    imageSrc: "/images/sembalun.jpg",
+    title: "Sembalun Village",
+    location: "East Lombok, West Nusa Tenggara, Indonesia",
+    description: `Sembalun Village, located on the eastern side of Lombok Island,
+      Indonesia, is a picturesque highland area renowned for its
+      stunning natural beauty and cultural richness. Nestled at the
+      foot of Mount Rinjani, this village boasts lush green valleys,
+      rolling hills, and expansive rice fields that create a
+      breathtaking landscape.`,
+    reviews: [
+      {
+        name: "Andres Fernando",
+        text: `Sembalun Village in Lombok Island, Indonesia, is a hidden
+          gem with breathtaking landscapes. Nestled in the mountains,
+          it offers stunning views, vibrant culture, and serene hiking
+          trails.`,
+      },
+      {
+        name: "Michelle Henderson",
+        text: `Sembalun Village on Lombok Island, Indonesia, is absolutely
+          magical! The lush green hills and clear blue skies are like
+          something out of a dream. The hiking trails are incredible,
+          and the friendly locals make you feel so welcome.`,
+      },
+    ],
+  },
+  {
+    id: 2,
+    imageSrc: "/images/rinjani.jpg",
+    title: "Rinjani Mountain",
+    location: "East Lombok, West Nusa Tenggara, Indonesia",
+    description: `Mount Rinjani, the majestic crown jewel of Lombok Island,
+      Indonesia, stands as a testament to the island natural grandeur.
+      Soaring to an impressive height of 3,726 meters, Rinjani is the
+      second highest volcano in Indonesia, offering breathtaking
+      panoramic views that captivate the soul. Its verdant slopes are
+      adorned with lush forests, cascading waterfalls, and vibrant
+      flora, creating a paradise for nature enthusiasts and
+      adventurers alike.`,
+    reviews: [
+      {
+        name: "Emily Johnson",
+        text: `My trek up Mount Rinjani was an unforgettable adventure! The
+          views from the summit are absolutely breathtaking, and
+          watching the sunrise from the peak was magical. The hike was
+          challenging but totally worth it.`,
+      },
+      {
+        name: "Michael Lee",
+        text: `Climbing Mount Rinjani was a life-changing experience. The
+          journey through diverse landscapes, from lush forests to the
+          rugged summit, was incredible. The panoramic views at the
+          top are simply out of this world.`,
+      },
+    ],
+  },
+  {
+    id: 3,
+    imageSrc: "/images/senggigi.jpg",
+    title: "Senggigi Beach",
+    location: "West Lombok, West Nusa Tenggara, Indonesia",
+    description: `Senggigi Beach, located on the western coast of Lombok Island,
+      Indonesia, is a serene and captivating destination renowned for
+      its natural beauty and tranquil ambiance. This picturesque beach
+      features a long stretch of soft, golden sand gently kissed by
+      the crystal-clear turquoise waters of the Sea. The beach is
+      perfect and people were kind.`,
+    reviews: [
+      {
+        name: "Sophia Martinez",
+        text: `Senggigi Beach is an absolute gem on Lombok Island! The
+          sunsets here are some of the most beautiful Ive ever seen,
+          with the sky turning vibrant shades of orange and pink. The
+          beach itself is pristine, with soft white sand and
+          crystal-clear waters perfect for swimming and snorkeling.`,
+      },
+      {
+        name: "David Wilson",
+        text: `I had a fantastic time at Senggigi Beach! The coastline is
+          stunning, and the water is so clear and inviting. Its a
+          great spot for water sports, and I enjoyed snorkeling and
+          paddleboarding. The local vendors are friendly and offer a
+          variety of interesting souvenirs and tasty snacks.`,
+      },
+    ],
+  },
+  {
+    id: 4,
+    imageSrc: "/images/gili-trawangan.jpg",
+    title: "Gili Trawangan",
+    location: "North Lombok, West Nusa Tenggara, Indonesia",
+    description: `Gili Trawangan, the largest of the three Gili Islands off the
+      northwest coast of Lombok, Indonesia, is a tropical paradise
+      known for its vibrant atmosphere and stunning natural beauty.
+      Surrounded by crystal-clear turquoise waters and fringed with
+      white sandy beaches, Gili Trawangan is a haven for beach lovers,
+      divers, and adventure seekers.`,
+    reviews: [
+      {
+        name: "Jessica Thompson",
+        text: `Gili Trawangan is simply amazing! The beaches are stunning,
+          with clear blue waters perfect for snorkeling and diving. I
+          saw so many beautiful fish and even a few turtles! The
+          island has a great vibe, with lots of cool beach bars and
+          restaurants.`,
+      },
+      {
+        name: "Ryan Davis",
+        text: `I had the best time on Gili Trawangan! The island is
+          gorgeous, and the water is so clear perfect for diving and
+          snorkeling. The local dive shops are great and offer
+          fantastic tours. I enjoyed the laid-back atmosphere during
+          the day and the lively nightlife in the evening.`,
+      },
+    ],
+  },
+];
