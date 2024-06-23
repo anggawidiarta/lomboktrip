02dde42429a4a42e2a6f29ba4fbc08b0ad1c6ce7
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-23 22:24:00 +0800

    style: adjust footer to fit with dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      <h4 className="bold-18 whitespace-nowrap dark:text-green-50">{title}</h4>
       {children}
     </div>
   );
@@ -27,7 +27,7 @@ const Footer = () => {
       <div className="flex flex-col w-full gap-10">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <p className="text-3xl poppins-bold">
+            <p className="text-3xl text-black poppins-bold dark:text-white">
               TripIn
               <span className="transition-all duration-150 text-green-50 hover:text-green-700">
                 Lombok.
@@ -38,7 +38,7 @@ const Footer = () => {
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns, index) => (
               <FooterColumn title={columns.title} key={index}>
-                <ul className="flex flex-col gap-4 regular-14 text-gray-30">
+                <ul className="flex flex-col gap-4 regular-14 text-gray-30 dark:text-white">
                   {columns.links.map((link) => (
                     <Link
                       href="/"
@@ -60,8 +60,10 @@ const Footer = () => {
                     key={link.label}
                     className="flex items-center gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className=" whitespace-nowrap">{link.label}:</p>
-                    <p className="transition-all duration-150 hover:text-green-50 medium-14 whitespace-nowrap text-blue-70">
+                    <p className=" whitespace-nowrap dark:text-white">
+                      {link.label}:
+                    </p>
+                    <p className="transition-all duration-150 dark:text-green-50 dark:hover:text-green-700 hover:text-green-50 medium-14 whitespace-nowrap text-blue-70">
                       {link.value}
                     </p>
                   </Link>
@@ -77,6 +79,7 @@ const Footer = () => {
                       <Image
                         src={link}
                         alt="logo"
+                        className="dark:fill-green-50"
                         width={24}
                         height={24}
                         loading="lazy"
@@ -90,7 +93,7 @@ const Footer = () => {
         </div>
 
         <div className="border bg-gray-20" />
-        <p className="w-full text-center regular-14 text-gray-30">
+        <p className="w-full text-center regular-14 text-gray-30 dark:text-white">
           {new Date().getFullYear()} TripInLombok | All rights reserved
         </p>
       </div>
